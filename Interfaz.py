cb7fef75d4c36f47f39c8219f62ca93b4e20e0cd
Maximo <maximors2110@gmail.com>
2025-05-01 03:02:51 -0300

    Agregado de botones

--- Interfaz.py
+++ Interfaz.py
@@ -2,12 +2,25 @@ import tkinter
 
 # Ventana tk
 ventana = tkinter.Tk()
-ventana.geometry("600x500")
+ventana.geometry("650x400")
 
-# Creación de título como etiqueta
-etiqueta_titulo = tkinter.Label(ventana, text = "MINI GESTOR DE TAREAS", bg = "light grey")
+# Creación de título como label
+etiqueta_titulo = tkinter.Label(ventana, 
+                                text = "MENÚ GESTOR DE TAREAS",
+                                bg = "light grey",
+                                height = 2,
+                                font = ("Arial", 20, "bold"))
 etiqueta_titulo.pack(fill = tkinter.X)
 
+# Creación de Botones para funciones principales
+boton_agregar = tkinter.Button(ventana, text = "Agregar")
+boton_agregar.pack()
+boton_estado = tkinter.Button(ventana, text = "Cambiar Estado")
+boton_estado.pack()
+boton_eliminar = tkinter.Button(ventana, text = "Eliminar")
+boton_eliminar.pack()
+boton_vista = tkinter.Button(ventana, text = "Ver tareas")
+boton_vista.pack()
 
 
 ventana.mainloop()
