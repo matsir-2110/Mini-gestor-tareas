1c6d5d9db679d9ef897497fb0d13e96a7d1dcfa4
Maximo <maximors2110@gmail.com>
2025-04-30 22:25:54 -0300

    Finalizada función que imprime las tareas

--- Gestor_de_tareas.py
+++ Gestor_de_tareas.py
@@ -1,14 +1,13 @@
-# Integrantes Costantini M. Mortola S. Repka M.
 tarea = {
-    1: {"Tarea": "Estudiar para los parciales de la semana que viene", "descripcion": "", "completada": False},
-    2: {"Tarea": "Ir al gimnasio todos los dias", "descripcion": "", "completada": False},
-    3: {"Tarea": "Leer libros 3 horas por dia", "descripcion": "", "completada": False},
-    4: {"Tarea": "Practicar tiros al arco de futbol", "descripcion": "", "completada": False}
+    1: {"Tarea": "Estudiar para los parciales de la semana que viene", "Descripcion": "", "Completada": "Pendiente"},
+    2: {"Tarea": "Ir al gimnasio todos los dias", "Descripcion": "", "Completada": "Pendiente"},
+    3: {"Tarea": "Leer libros 3 horas por dia", "Descripcion": "", "Completada": "Pendiente"},
+    4: {"Tarea": "Practicar tiros al arco de futbol", "Descripcion": "", "Completada": "Pendiente"}
 }
 
 def agregado():
     nueva_tarea = input("Escriba la tarea que desea agregar:  ")
-    opcion = input("¿Desea agregar alguna descripcion?").lower()
+    opcion = input("¿Desea agregar alguna descripcion? ").lower()
 
     if opcion == 'si':
         descripcion = input("Ingrese la descripción de la tarea:  ")
@@ -28,33 +27,33 @@ def agregado():
 #def eliminado():
     # Agregar prox
 
-#def estados():
-    # Agregar prox
-
-#def mostrar_tareas():
-    # Agregar pro
+def listado():
+    i = 1
+    for i, datos in tarea.items():
+        print("TAREA NÚMERO: ", i)
+        print("Tarea: ", datos['Tarea'])
+        print("Descripción: ", datos['Descripcion'])
+        print("Estado: ", datos['Completada'])
+        print("")
 
 def menus():
     print("\nMenu de tareas. Seleccione que desea hacer:")
     print("Agregar una tarea con descripción (AGREGAR)")
     print("Marcar una tarea como completada (MARCAR)")
     print("Eliminar una tarea (ELIMINAR)")
-    print("Estado de las tareas. Completada / Pendiente (ESTADO)")
-    print("Mostrar tareas (MOSTRAR)")
+    print("Vista de las tareas con su estado. Completada / Pendiente (LISTADO)")
     menu = input().lower()
 
     if menu == 'agregar':
         agregado()
-    if menu == 'marcar':
-        marcado()
-    if menu == 'eliminar':
-        eliminado()
-    if menu == 'estado':
-        estados()
-    if menu == 'mostrar':
-        mostrar_tareas()
+    #if menu == 'marcar':
+    #    marcado()
+    #if menu == 'eliminar':
+    #    eliminado()
+    if menu == 'listado':
+        listado()
 menus()
 
-opcion2 = input("¿Desea volver a seleccionar alguna de las opciones anteriores?").lower()
+opcion2 = input("¿Desea volver a seleccionar alguna de las opciones anteriores? ").lower()
 if opcion2 == 'si':
     menus()
